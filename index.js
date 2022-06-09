3376d686e04dd7a38e4b66bd827c72817b10fd05
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-06-09 12:08:26 +0530

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+// alert('this')
+console.time('my')
+console.log('hello');
+console.log(34+6);
+console.log([2,45,78,96]);
+console.log({h:'this',m:56})
+// console.table({h:'this',m:56})
+// console.warn(45)
+// console.clear()
+// console.timeEnd('my')
+// console.assert(856<189,'this is not posssible')
+// console.error('yo')
