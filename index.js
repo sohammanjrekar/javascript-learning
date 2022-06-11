e49359ca122c0812714f09e90fe05d5ff42911e6
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-06-11 14:58:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,55 @@ console.log({h:'this',m:56})
 // console.timeEnd('my')
 // console.assert(856<189,'this is not posssible')
 // console.error('yo')
+
+
+// Variables in js
+
+
+// var, let , const used to create variable
+ var city='harry'
+ console.log(name)
+var l;
+var marks=569
+console.log(name,marks,l)
+
+// Rules for creating javascript variables
+// 1) cannot start with Number
+// 2) can start with letter , _  oR $
+// 3) Are case sensitive
+
+
+const oname='lek';
+// oname="hai"; //cannot do this
+console.log(oname);
+{
+let city='rampur';
+city='ko'
+console.log(city);
+}
+console.log(city);
+
+const arr1=[1,5,6,8,98,8]
+arr1.push(2) //add in last
+arr1.unshift(58)//add in first
+console.log(arr1);
+arr1.pop()// delete last number
+arr1.shift()//delete first number
+console.log(arr1);
+
+// most common programming case types
+// 1. camelCase -used mostly
+// 2. kebab-case
+// 3. snake_case
+// 4.PascalCase
+
+// datatypes in JS
+// 1) primitive datatype -stack
+//     a) String
+//     b) numbers
+//     c) Boolean
+//     d) null]
+//     e)undefined
+//     f) Symbol
+// 2) REfernse datatype -heap
+    a)
