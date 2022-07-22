8f48d0c9dfe2cb891a75e4be40b8ee84ccd2ff99
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-07-22 17:35:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -807,10 +807,15 @@
 
 
 //await / async
-async function harry(){
-    return "harry";
+// asysn retuen promise
+// async function harry(){
+// const response=await fetch('https://api.github.com/users')
+// const users=await response.json();
+// return users;
+// }
+// let a= harry();
+// console.log(a);
+// a.then(data=>console.log(data))
 
-}
-let a= harry();
-console.log(a);
 
+//
