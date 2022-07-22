ec3c20f7919266fcda854b60848f875af1d303b3
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-07-22 13:43:40 +0530

    update

---
+++ project3/index.js
@@ -0,0 +1,16 @@
+// https://newsapi.org/v2/top-headlines?country=in&apiKey=faa4be120672496ab2d47b2a2de16ebe
+// news api key : faa4be120672496ab2d47b2a2de16ebe
+
+//Example request
+// var url = 'https://newsapi.org/v2/everything?' +
+// 'q=Apple&' +
+// 'from=2022-07-22&' +
+// 'sortBy=popularity&' +
+// 'apiKey=faa4be120672496ab2d47b2a2de16ebe';
+
+// var req = new Request(url);
+
+// fetch(req)
+// .then(function(response) {
+// console.log(response.json());
+// })
