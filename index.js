0a96bf92414c23a84b97879270197e7da447c29e
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-07-24 18:27:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -968,10 +968,13 @@ let s="this is great code with harry snd harry";
 
 function* numberGen() {
     let i=0;
-    yield 1;
-    yield 2;
-    yield 3;
-    yield 4;
+    // yield 1;
+    // yield 2;
+    // yield 3;
+    // yield 4;
+    while (true) {
+        yield i++;
+    }
 }
 const gen=numberGen()
 console.log(gen.next());
